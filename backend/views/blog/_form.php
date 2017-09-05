e4faf88887197da70865f6ef5a8056cbf515df63
hanxiaoqing123 <1695722217@qq.com>
2017-09-05 22:27:00 +0800

    rbac（上）

---
+++ backend/views/blog/_form.php
@@ -0,0 +1,39 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+use yii\helpers\Url;
+/* @var $this yii\web\View */
+/* @var $model common\models\Blog */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="blog-form">
+
+    <?php $form = ActiveForm::begin([
+    'id' => 'blog-id',
+    'enableAjaxValidation' => true,
+    'validationUrl' => Url::toRoute(['validate-form']),
+    ]); ?>
+
+    <?= $form->field($model, 'id')->textInput() ?>
+
+    <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'content')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'views')->textInput() ?>
+
+    <?= $form->field($model, 'is_delete')->textInput() ?>
+
+    <?= $form->field($model, 'created_at')->textInput() ?>
+
+    <?= $form->field($model, 'updated_at')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
